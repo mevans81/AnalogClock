21439a7858171c19d3098869ed9eea51e02feffd
mevans81 <mevans@hampton.k12.va.us>
2023-10-20 15:46:45 -0400

    Ver 1

---
+++ app/src/main/java/analogclock/analogclock.java
@@ -0,0 +1,41 @@
+
+package analogclock;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+ 
+public class analogclock extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+ 
+ Circle circle = new Circle(100,100,100,Color.RED);
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(circle);
+        root.getChildren().add(btn);
+        
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
